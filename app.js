84cf89c29a8da34873ab2d2b21477c685bec9c4a
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-09-24 10:00:02 +0200

    Setup yargs arguments handler

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
-const fs = require('fs');
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes');
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js');
+// Customize yargs version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (argv) {
+    console.log('Title: ' + argv.title);
+    console.log('Body: ' + argv.body);
+  },
+});
+
+// Create remove note
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing the note...');
+  },
+});
+
+// Read the note
+yargs.command({
+  command: 'read',
+  describe: 'Read the note...',
+  handler: function () {
+    console.log('Reading the note');
+  },
+});
+
+// Create list
+yargs.command({
+  command: 'list',
+  describe: 'Notes list',
+  handler: function () {
+    console.log('Listing the notes...');
+  },
+});
+// add, remove, read, list
+
+yargs.parse();
